19e59ea588adfef78596eee8d2efa6c810457276
farah moataz <farahmoataz90@gmail.com>
2024-12-01 19:51:30 +0200

    added login and signup pages

---
+++ angular-project/src/app/app.routes.ts
@@ -0,0 +1,27 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
+
+export const routes: Routes = [
+
+  {
+    path : 'login',
+    component: LoginComponent
+  },
+  {
+    path: "signup",
+    component: SignupComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: '' , redirectTo : 'home' , pathMatch: 'full'
+  },
+  {
+    path: '**', redirectTo: '', pathMatch: 'full'
+  }
+
+];
---
+++ angular-project/src/app/components/signup/signup.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+export class SignupComponent {
+  constructor() {
+    console.log('SignupComponent loaded');
+  }
+}
