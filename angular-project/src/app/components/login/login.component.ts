f6347e8eef37b9e9bca32c844a9e9f4ba5c2b12a
farah moataz <farahmoataz90@gmail.com>
2024-12-02 23:39:47 +0200

    validation of the login and  signup is done

--- angular-project/src/app/components/login/login.component.ts
+++ angular-project/src/app/components/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule], // Include CommonModule here
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  get email() {
+    return this.loginForm.controls['email'];
+  }
+
+  get password() {
+    return this.loginForm.controls['password'];
+  }
 }
--- angular-project/src/app/components/signup/signup.component.ts
+++ angular-project/src/app/components/signup/signup.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { passwordMatchValidator } from '../../shared/password-match.directive';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
-  constructor() {
-    console.log('SignupComponent loaded');
+  registerForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    confirmpassword: ['', Validators.required]
+  },{
+    validators: passwordMatchValidator
+  });
+  constructor(private fb:FormBuilder) {}
+
+  get email() {
+    return this.registerForm.controls['email'];
+  }
+
+  get password() {
+    return this.registerForm.controls['password'];
+  }
+
+  get confirmpassword()
+  {
+    return this.registerForm.controls['confirmpassword'];
+
   }
 }
