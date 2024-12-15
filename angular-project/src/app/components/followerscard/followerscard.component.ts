c6f1954d50d53f17455fe36333fdf6b10c7eafc6
farah moataz <farahmoataz90@gmail.com>
2024-12-16 01:28:06 +0200

    everything about followers and following is done

--- angular-project/src/app/components/followerscard/followerscard.component.ts
+++ angular-project/src/app/components/followerscard/followerscard.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input , Output, EventEmitter } from '@angular/core';
+import { Component, Input , Output, EventEmitter, OnInit  } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -8,7 +8,7 @@ import { RouterModule } from '@angular/router';
   templateUrl: './followerscard.component.html',
   styleUrl: './followerscard.component.scss'
 })
-export class FollowerscardComponent {
+export class FollowerscardComponent implements OnInit{
   @Input() mainImage!: string;
   @Input() name!: string;
   @Input() position!: string;
@@ -20,10 +20,10 @@ export class FollowerscardComponent {
   @Input() image5!: string;
 
 
-  // @Output() follow = new EventEmitter<any>();
 
-  // onFollow() {
-  //   const cardData = {
+  // addToFollowing() {
+  //   const currentFollowList = JSON.parse(sessionStorage.getItem('followingList') || '[]');
+  //   const newFollower = {
   //     mainImage: this.mainImage,
   //     name: this.name,
   //     position: this.position,
@@ -34,34 +34,49 @@ export class FollowerscardComponent {
   //     image4: this.image4,
   //     image5: this.image5,
   //   };
-  //   this.follow.emit(cardData);
+
+  //   // Check if already exists
+  //   const alreadyExists = currentFollowList.some(
+  //     (follower: any) => follower.name === newFollower.name
+  //   );
+
+  //   if (!alreadyExists) {
+  //     currentFollowList.push(newFollower);
+  //     sessionStorage.setItem('followingList', JSON.stringify(currentFollowList));
+  //   }
   // }
 
-  addToFollowing() {
+
+  isFollowing = false; // Tracks if the user is following this person
+
+  ngOnInit() {
+    // Check if the current card is already in the following list
     const currentFollowList = JSON.parse(sessionStorage.getItem('followingList') || '[]');
-    const newFollower = {
-      mainImage: this.mainImage,
-      name: this.name,
-      position: this.position,
-      num: this.num,
-      image1: this.image1,
-      image2: this.image2,
-      image3: this.image3,
-      image4: this.image4,
-      image5: this.image5,
-    };
+    this.isFollowing = currentFollowList.some((follower: any) => follower.name === this.name);
+  }
 
-    // Check if already exists
-    const alreadyExists = currentFollowList.some(
-      (follower: any) => follower.name === newFollower.name
-    );
-
-    if (!alreadyExists) {
+  toggleFollowing() {
+    const currentFollowList = JSON.parse(sessionStorage.getItem('followingList') || '[]');
+    if (this.isFollowing) {
+      // Remove from the list
+      const updatedList = currentFollowList.filter((follower: any) => follower.name !== this.name);
+      sessionStorage.setItem('followingList', JSON.stringify(updatedList));
+    } else {
+      // Add to the list
+      const newFollower = {
+        mainImage: this.mainImage,
+        name: this.name,
+        position: this.position,
+        num: this.num,
+        image1: this.image1,
+        image2: this.image2,
+        image3: this.image3,
+        image4: this.image4,
+        image5: this.image5,
+      };
       currentFollowList.push(newFollower);
       sessionStorage.setItem('followingList', JSON.stringify(currentFollowList));
     }
+    this.isFollowing = !this.isFollowing; // Toggle the state
   }
-
-
-
 }
